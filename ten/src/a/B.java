4068b578642e9755feaf80b5e6ff6450188ef10f
HP <HP@Siyaram>
2023-08-26 10:46:39 +0530

    Hello M a gaya

---
+++ ten/src/a/B.java
@@ -0,0 +1,11 @@
+package a;
+
+public class B {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Prashant");
+		System.out.println("Johnny");
+		System.out.println("Nitin");
+	}
+}
